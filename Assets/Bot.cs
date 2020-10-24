bb32a051711b560d77f160ad106714564a0d3622
Peter <picusbrit@gmail.com>
2020-10-24 17:07:30 -0400

    L33 - Initial scripting of flee and seek behaviours.

---
+++ Assets/Bot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bot : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Seek(Vector3 targetLocation)
+    {
+        agent.SetDestination(targetLocation);
+    }
+
+    void Flee(Vector3 targetLocation)
+    {
+        Vector3 fleeLocation = transform.position - (targetLocation - transform.position);
+        agent.SetDestination(fleeLocation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Flee(target.transform.position);
+    }
+}
